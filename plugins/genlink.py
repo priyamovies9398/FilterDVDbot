61f0f451bed389c8df34519737c9779f1884fdff
PriyaBots <158731094+priyamovies9398@users.noreply.github.com>
2024-07-30 02:02:30 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -36,10 +36,10 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/VJ_Botz/10 https://t.me/VJ_Botz/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/PriyaMovis/10 https://t.me/PriyaMovis/20</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/VJ_Botz/10 https://t.me/VJ_Botz/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/PriyaMovis/10 https://t.me/PriyaMovis/20</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
@@ -69,7 +69,7 @@ async def gen_link_batch(bot, message):
     except Exception as e:
         return await message.reply(f'Errors - {e}')
 
-    sts = await message.reply("Generating link for your message.\nThis may take time depending upon number of messages")
+    sts = await message.reply("Generating link MAWA PLZ WAITU.\nThis may take time depending upon number of messages")
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
